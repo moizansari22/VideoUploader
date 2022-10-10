f62363e70a5ce5552a242b838ea1e422919c9846
Moiz's Macbook <apple@Moizs-MacBook-Pro.local>
2022-10-10 19:18:34 +0500

    Video Uploading functionality

---
+++ db/migrate/20221010112725_create_videos.rb
@@ -0,0 +1,11 @@
+class CreateVideos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.text :introduction
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
